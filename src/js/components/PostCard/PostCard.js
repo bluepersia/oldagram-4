2264cb1e9e0cc70a5bb3e0399b734a9d5be100fe
Marco Zimmermann <fuzzyslash@hotmail.com>
2026-03-18 17:02:34 -0600

    Main feed JS functionality

---
+++ src/js/components/PostCard/PostCard.js
@@ -0,0 +1,27 @@
+import { generateHTML, toggleLike } from "./utils.js";
+
+export default function PostCard(root, post) {
+  let state = {
+    isLiked: false,
+    likes: post.likes,
+  };
+
+  root.innerHTML = generateHTML(post);
+
+  const imgEl = root.querySelector("[data-img]");
+  const likeBtn = root.querySelector("[data-like]");
+  const likesEl = root.querySelector("[data-likes]");
+
+  imgEl.addEventListener("dblclick", handleLikeClick);
+  likeBtn.addEventListener("click", handleLikeClick);
+
+  function handleLikeClick() {
+    state = toggleLike(state);
+
+    renderLike();
+  }
+
+  function renderLike() {
+    likesEl.textContent = `${state.likes} likes`;
+  }
+}
